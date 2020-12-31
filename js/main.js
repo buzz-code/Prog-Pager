856689cfcbc9cf18083865c0cf6d4e8ab649bf46
buzz-code <72219643+buzz-code@users.noreply.github.com>
2020-12-31 22:04:52 +0200

    improve code

--- js/main.js
+++ js/main.js
@@ -13,6 +13,9 @@ function getThread() {
 	var threadId = location.href.match(/threads\/[^\.]*\.(\d*)/)[1];
 	var url = location.origin + '/threads/' + threadId;
 	var data = $('<html>');
+	data.append('<base href="' + window.location.href + '">');
+	data.append('<style>body {direction: rtl} .message-main {height: unset !important}</style>');
+	data.append('<style>[data-widget-definition="xa_ams_latest_articles"],[data-position="big_main_banner"],.p-body-sidebar,.block.block--messages .block-outer,form,.blockMessage.blockMessage--none,.p-breadcrumbs--bottom {display: none !important}</style>');
 	$('<span class="globalAction"><span class="globalAction-bar" /></span>')
 	.appendTo("body")
 	.addClass("is-active");
@@ -21,7 +24,7 @@ function getThread() {
 		$.get(url + '/page-' + page)
 		.done(function(response) {
 			var $res = $(response);
-			appendToPreviousReponse($res);
+			data.append($res);
 			var pageEl = $res.find('.pageNavSimple-el--next').attr('href');
 			if(pageEl) {
 				var page = pageEl.match(/page-(\d*)/)[1];
@@ -35,17 +38,11 @@ function getThread() {
 			completeScraping();
 		});
 	}
-	function appendToPreviousReponse(html) {
-		data.append(html);
-	}
 	function completeScraping() {
 		$('.globalAction').remove();
-		data.find('[data-widget-definition="xa_ams_latest_articles"],[data-position="big_main_banner"],.p-body-sidebar,.block.block--messages .block-outer,form,.blockMessage.blockMessage--none,.p-breadcrumbs--bottom,footer.p-footer:not(:last),header.p-header:not(:first),.p-navSticky:not(:first),.p-sectionLinks:not(:first),.p-breadcrumbs:not(:first),.p-body-header:not(:first)').remove();
+		data.find('footer.p-footer:not(:last),header.p-header:not(:first),.p-navSticky:not(:first),.p-sectionLinks:not(:first),.p-breadcrumbs:not(:first),.p-body-header:not(:first)').remove();
 		var win = window.open("", Math.random(), "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes");
-		win.document.body.innerHTML = '<base href="' + window.location.href + '">';
-		win.document.body.innerHTML += '<style>body {direction: rtl}</style>';
-		win.document.body.innerHTML += data.html();
-		win.document.querySelectorAll('.message-main').forEach(function (item){item.style.height = 'unset';})
+		win.document.body.innerHTML = data.html();
 		win.focus();
 		win.print();
 		win.close();
